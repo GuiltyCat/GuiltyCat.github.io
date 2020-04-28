39b324c72c8c9d508189d04618670bc3f8390123
GuiltyCat <37097820+GuiltyCat@users.noreply.github.com>
2020-04-28 19:52:37 +0900

    update

--- index.bash
+++ index.bash
@@ -7,7 +7,7 @@ cat <<EOF
 更新(Updates)
 ----------
 
-$(tac "${HIST_FILE_NAME}" | grep "^-" | sed -e "s/\.\././" | head -n 10)
+$(tail -n +4 "${HIST_FILE_NAME}" | head -n 10 | sed -e "s/\.\././")
 - [全ての履歴(All History)](${HIST_FILE_NAME//md/html})
 
 
