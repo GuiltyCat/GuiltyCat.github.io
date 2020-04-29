d50231035823aa8d2d1ec64d023485f02148b648
GuiltyCat <miyamotokohei@protonmail.com>
2020-04-29 21:38:15 +0900

    update

--- article.bash
+++ article.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function diary_list() {
+function ArticleList() {
 	COUNTER=0
 	for FILE in $(find ./md -type f | sort -n); do
 		echo "${COUNTER}:${FILE}:$(head -n1 ${FILE})"
@@ -8,17 +8,18 @@ function diary_list() {
 	done
 }
 
-function help() {
+function Help() {
 	cat <<EOF
-$(basename $0) [--help | -h] [--list | -l] [--write | -w <NUM>]
+$(basename $0) [-lw] [--help | -h] [--list | -l] [--write | -w <NUM>]
 
 If no option passed, today's articles opens.
+I only use -lw option.
 
 --help, -h
 	Print this help.
 
 --list, -l
-	Print all articles' number and title.
+	Print all articles' number, file name and title list.
 	For example, 
 
 	0:md/0000-00-00.md History.
@@ -29,25 +30,43 @@ If no option passed, today's articles opens.
 
 --write, -w <NUM>
 	Write article of <NUM> that appears in --list option.
+
+--lw
+	mixture of list and write.
+	After listing, enter a number you want to write.
+	
+
 EOF
+}
+
+function Write(){
+	NUM=$1
+		FILE="$(find ./md -type f | sort -n | tail -n+$((NUM + 1)) | head -n 1)"
+		nvim "${FILE}"
 }
 
 while [[ $# -ne 0 ]]; do
 
 	case $1 in
 	--help | -h)
-		help
+		Help
 		exit
 		;;
 	--list | -l)
-		diary_list
+		ArticleList
 		exit
 		;;
 	--write | -w)
 		shift
 		NUM=$1
-		FILE="$(find ./md -type f | sort -n | tail -n+$((NUM + 1)) | head -n 1)"
-		nvim "${FILE}"
+		Write ${NUM}
+		exit
+		;;
+	-lw )
+		ArticleList
+		echo -n "Number to write:"
+		read NUM
+		Write ${NUM}
 		exit
 		;;
 	*) ;;
