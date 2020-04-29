f1f0269eb77e4fbbc67da951efb0d2bbe13a6b96
GuiltyCat <miyamotokohei@protonmail.com>
2020-04-29 19:21:41 +0900

    update

--- diary.bash
+++ diary.bash
@@ -1,6 +1,32 @@
 #!/bin/bash
 
+function diary_list() {
+	COUNTER=0
+	for FILE in $(find ./md -type f | sort -n); do
+		echo "${COUNTER}:${FILE}:$(head -n1 ${FILE})"
+		COUNTER=$((COUNTER+1))
+	done
+}
+
+while [[ $# -ne 0 ]]; do
+
+	case $1 in
+	--list | -l)
+		diary_list
+		exit
+		;;
+	--write | -w)
+		shift
+		NUM=$1
+		FILE="$(find ./md -type f | sort -n | tail -n+$((NUM+1)) | head -n 1)"
+		nvim "${FILE}"
+		exit
+		;;
+	*) ;;
+	esac
+	shift
+done
+
 TODAY=$(date +%Y-%m-%d)
 
 nvim "./md/${TODAY}.md"
-
