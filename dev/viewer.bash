8d38cfa47847e11b6929188900362958c3e198cc
GuiltyCat <37097820+GuiltyCat@users.noreply.github.com>
2020-04-30 22:38:13 +0900

    update

--- dev/viewer.bash
+++ dev/viewer.bash
@@ -4,10 +4,14 @@ RIGHT=$2
 cat <<EOF
 <div id="left">
 <iframe src="${LEFT}" name="left">
+あいうえお
 </iframe>
 </div>
+<div id="right">
 <iframe src="${RIGHT}" name="right">
-# </iframe>
+asdfasdfasdf
+</iframe>
+</div>
 EOF
 # <div id="right">
 # </div>
