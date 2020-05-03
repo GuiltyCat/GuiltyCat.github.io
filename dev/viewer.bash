2f6080cdbf6c4a4a65202fe0f38824f6a2fbcfe8
GuiltyCat <miyamotokohei@protonmail.com>
2020-05-03 10:48:32 +0900

    update

--- dev/viewer.bash
+++ dev/viewer.bash
@@ -4,15 +4,16 @@ cat <<EOF
 <dev id="flex">
 <div class="list">
 <div class="inner_list">
-$(for i in $(seq 100);do
-echo "<a href=id=\"${i}\">List of pages.</a>"
+$(for i in $(seq 10);do
+echo "<a href=\"img/${i}.html\" id=\"${i}\" target=\"image\">${i}</a>"
 done
 )
 </div>
 </div>
 <div>
-<iframe src="${RIGHT}" name="page">
+<iframe src="${RIGHT}" name="image">
 </iframe>
 </div>
 </div>
 EOF
+# <iframe src="${RIGHT}" name="image">
