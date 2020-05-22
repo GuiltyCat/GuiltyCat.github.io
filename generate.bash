6c2d23cf8633953d65ee16678644ad4dc50e2093
GuiltyCat <37097820+GuiltyCat@users.noreply.github.com>
2020-05-22 20:17:14 +0900

    update

--- generate.bash
+++ generate.bash
@@ -5,6 +5,19 @@ function RemoveNewline() {
 	LC_CTYPE=UTF-8 sed -z "${SED_EXP}" $@
 }
 
+function TableOfContents() {
+	FILE=$(cat -)
+	TOC=$(echo "$FILE" | grep -B1 -e "^[-]\+$" | sed -e 's/^[-]\+$//' | sed -e '/^$/d' | sed -e 's/^/- /')
+	head -n 3 <<<"${FILE}"
+	cat <<EOF
+目次 (Table of Contents)
+---------------------
+
+${TOC}
+EOF
+	tail -n +3 <<<"${FILE}"
+}
+
 TOP_LINK="../index.html"
 TOP_NAME="トップ(TOP)"
 
@@ -36,21 +49,20 @@ while [[ "$#" -ne 0 ]]; do
 	shift
 done
 
-
 TITLE=$(echo "$FILE" | head -n 1)
 if [[ "${TITLE}" == *:*Manga* ]]; then
 	COUNTER=0
 	DIR_NAME="html/$(basename ${NAME%.*})"
 	LINK_NAME=$(basename ${NAME%.*})
 	mkdir -p "${DIR_NAME}"
-	IMG_LIST=$(grep "http" <<<${FILE}|sed -e 's/[^\[]*\[.*\](\(http[^)]*\)).*/\1\n/g')
-	MAX_NUM=$(echo "${IMG_LIST}" |wc -l)
+	IMG_LIST=$(grep "http" <<<${FILE} | sed -e 's/[^\[]*\[.*\](\(http[^)]*\)).*/\1\n/g')
+	MAX_NUM=$(echo "${IMG_LIST}" | wc -l)
 	IFS='
 	'
 	for IMG_URL in $(echo ${IMG_LIST}); do
 		COUNTER=$((COUNTER + 1))
 		FILE_NAME=$(bash generate_comic.bash --file_name --max "${MAX_NUM}" --num "${COUNTER}")
-		bash $0 --top-link "../$(basename ${DIR_NAME}).html" --top-name "記事(Article)" --css "${CSS}" --md <(bash generate_comic.bash --max "${MAX_NUM}" --num "${COUNTER}" --img-url "${IMG_URL}" ) >"${DIR_NAME}/${FILE_NAME}"
+		bash $0 --top-link "../$(basename ${DIR_NAME}).html" --top-name "記事(Article)" --css "${CSS}" --md <(bash generate_comic.bash --max "${MAX_NUM}" --num "${COUNTER}" --img-url "${IMG_URL}") >"${DIR_NAME}/${FILE_NAME}"
 		LINK_NUM=$(printf "%0${#MAX_NUM}d" ${COUNTER})
 		FILE=$(echo "${FILE}" | sed -e "s/(${IMG_URL//\//\\\/})/(${LINK_NAME//\//\\\/}\/${FILE_NAME})/")
 		echo "$FILE" >&2
@@ -67,7 +79,7 @@ cat <<EOF
 </head>
 <body>
 <a href="${TOP_LINK}">${TOP_NAME}</a>
-$(awk -f md2html.awk <(RemoveNewline <<<${FILE}))
+$(awk -f md2html.awk <(RemoveNewline <<<${FILE} | TableOfContents))
 <a href="${TOP_LINK}">${TOP_NAME}</a>
 </body>
 </html>
--- toc.bash
+++ toc.bash
@@ -1,3 +1,2 @@
 #!/bin/bash
 
-## cat - | grep -B1 -e "^[-]\+$" | sed -e 's/^[-]\+$//' |sed -e '/^$/d' | 
