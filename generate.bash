d081279356744c946de7a83c18ae397e4463cea5
GuiltyCat <37097820+GuiltyCat@users.noreply.github.com>
2024-10-19 00:01:05 +0900

    update

--- generate.bash
+++ generate.bash
@@ -75,6 +75,7 @@ cat <<EOF
 <html>
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>${TITLE}</title>
 <script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js" type="text/javascript"></script>
 </head>
